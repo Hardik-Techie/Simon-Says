15575f52d92d310f3963e972c30ab117151d0f34
Hardik Srivastava <144905418+Hardik-Techie@users.noreply.github.com>
2024-05-15 16:50:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-let startButton = document.getElementById("startButton");
 let gameSeq = [];
 let userSeq = [];
-
 let btns = ["red", "blue", "green", "yellow"];
 let started = false;
 let level = 0;
-let highestScore = localStorage.getItem("highestScore") || 0; // Retrieve highest score from local storage
+let highestScore = localStorage.getItem("highestScore") || 0;
 let h2 = document.querySelector("h2");
-
+let startButton = document.getElementById("startButton");
 
 document.addEventListener("keypress", startGame);
 document.addEventListener("touchstart", startGame);
 startButton.addEventListener("click", startGame);
 
 function startGame() {
-    if (!started) {
-        console.log("Game Started");
-        started = true;
-        levelUp();
-        
-        // Clear local storage when the game starts for a new user
-        localStorage.removeItem("highestScore");
-        highestScore = 0;
-    }
+  if (!started) {
+    console.log("Game Started");
+    started = true;
+    levelUp();
+  }
 }
 
 function gameFlash(btn) {
@@ -47,18 +41,45 @@ function levelUp() {
   let randIdx = Math.floor(Math.random() * 3);
   let randColor = btns[randIdx];
   let randBtn = document.querySelector(`.${randColor}`);
-  // console.log(randIdx);
-  // console.log(randColor);
-  // console.log(randBtn);
   gameSeq.push(randColor);
-  console.log(gameSeq);
   gameFlash(randBtn);
 }
 
 function checkAns(idx) {
   if (userSeq[idx] === gameSeq[idx]) {
     if (userSeq.length == gameSeq.length) {
-      if (level > highestScore) {
-        // Update highest score if current level is higher
+      if (level >= highestScore) {
         highestScore = level;
-        localStorag
+        localStorage.setItem("highestScore", highestScore);
+      }
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerHTML = `Game Over! <br>Your score : <b>${level}</b> <br> Highest Score : <b>${highestScore}</b><br>Press any key to Start`;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "#e5eaf5";
+    }, 150);
+    reset();
+  }
+}
+
+function btnPress() {
+  let btn = this;
+  userFlash(btn);
+  userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+  checkAns(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
+}
